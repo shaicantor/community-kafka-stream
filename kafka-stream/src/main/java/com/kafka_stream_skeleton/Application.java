1f646e5939d8ad31be47fd201c08eb483bea66d8
Haim Cohen <haim.co@moonactive.com>
2018-12-24 23:09:50 +0200

    Stream example application - alter output interval and window lengths

--- kafka-stream/src/main/java/com/kafka_stream_skeleton/Application.java
+++ kafka-stream/src/main/java/com/kafka_stream_skeleton/Application.java
@@ -38,7 +38,7 @@ public class Application {
         streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, APPLICATION_ID);
         streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVER);
         streamsConfiguration.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        streamsConfiguration.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 10 * 1000);
+        streamsConfiguration.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 100);
 
         Serde<LoginData> loginDataSerde = SerdeBuilder.buildSerde(LoginData.class);
 
@@ -48,13 +48,13 @@ public class Application {
         final KStream<String, LoginData> source = builder.stream(INPUT_TOPIC, Consumed.with(Serdes.String(), loginDataSerde));
 
 
-        System.out.println("start streaming processing on topic "+INPUT_TOPIC);
+        System.out.println("start streaming processing on topic " + INPUT_TOPIC);
 
         KTable<Windowed<String>, Long> counts = source
                 .filter((key, value) -> value != null)
                 .map((key, value) -> new KeyValue<>(value.getUserName(), value))
                 .groupByKey(Serialized.with(Serdes.String(), loginDataSerde))
-                .windowedBy(TimeWindows.of(TimeUnit.SECONDS.toMillis(1)))
+                .windowedBy(TimeWindows.of(TimeUnit.SECONDS.toMillis(30)))
                 .count();
 
 
@@ -62,10 +62,15 @@ public class Application {
 
         Serde<LoginCount> loginCountSerde = SerdeBuilder.buildSerde(LoginCount.class);
 
-        counts.toStream().map((windowed,count)->new KeyValue<>(windowed.key(),new LoginCount(windowed.key(),count,windowed.window().start(),windowed.window().end())))
+        counts
+                .toStream()
+                .map((windowed,count) ->
+                        new KeyValue<>(windowed.key(),new LoginCount(windowed.key(),
+                                count,windowed.window().start(),windowed.window().end())))
                 .to(OUTPUT_TOPIC, Produced.with(stringSerde, loginCountSerde));
 
-        System.out.println("streaming processing will be produced to topic "+OUTPUT_TOPIC);
+
+        System.out.println("Streaming processing will produce results to topic " + OUTPUT_TOPIC);
 
         return new KafkaStreams(builder.build(), streamsConfiguration);
     }
