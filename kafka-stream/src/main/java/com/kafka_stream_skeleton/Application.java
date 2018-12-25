d02ce9b7f07e551301bbcf4bcf7291a5ef8e92d8
Shai Cantor <shai@sealights.io>
2018-12-25 16:42:18 +0200

    kafka sealights streams template

--- kafka-stream/src/main/java/com/kafka_stream_skeleton/Application.java
+++ kafka-stream/src/main/java/com/kafka_stream_skeleton/Application.java
@@ -5,7 +5,14 @@ package com.kafka_stream_skeleton;
 import com.kafka_stream_skeleton.model.LoginCount;
 import com.kafka_stream_skeleton.model.LoginData;
 import com.kafka_stream_skeleton.model.proxy.MethodKeyHits;
+import com.kafka_stream_skeleton.model.proxy.SubmitBuildMappingRequest;
+import com.kafka_stream_skeleton.model.proxy.build.BuildMappingRequest;
+import com.kafka_stream_skeleton.model.proxy.build.MethodData;
 import com.kafka_stream_skeleton.model.proxy.events.SubmitEventsRequest;
+import com.kafka_stream_skeleton.model.proxy.footprints.AppData;
+import com.kafka_stream_skeleton.model.proxy.footprints.FileData;
+import com.kafka_stream_skeleton.model.proxy.footprints.FootprintsData;
+import com.kafka_stream_skeleton.model.proxy.footprints.SubmitFootprintsRequest;
 import com.kafka_stream_skeleton.serialization.SerdeBuilder;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serde;
@@ -16,7 +23,7 @@ import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.*;
 
-import java.util.Properties;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 public class Application {
@@ -42,32 +49,66 @@ public class Application {
         Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
     }
 
-    private static KafkaStreams buildSealightsStream() {
+    private static KafkaStreams buildSealightsStream2() {
         final Properties streamsConfiguration = new Properties();
         streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, APPLICATION_ID);
         streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVER);
         streamsConfiguration.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         streamsConfiguration.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 100);
 
-        Serde<Long> footprintsDataSerde = SerdeBuilder.buildSerde(Long.class);
+        Serde<SubmitFootprintsRequest> footprintsDataSerde = SerdeBuilder.buildSerde(SubmitFootprintsRequest.class);
         Serde<SubmitEventsRequest> testEventsDataSerde = SerdeBuilder.buildSerde(SubmitEventsRequest.class);
-        Serde<String> buildMappingDataSerde = SerdeBuilder.buildSerde(String.class);
+        Serde<BuildMappingRequest> buildMappingDataSerde = SerdeBuilder.buildSerde(BuildMappingRequest.class);
 
         final StreamsBuilder builder = new StreamsBuilder();
-        final KStream<String, Long> footprintsStream = builder.stream(FOOTPRINTS_TOPIC, Consumed.with(Serdes.String(), footprintsDataSerde));
+        final KStream<String, SubmitFootprintsRequest> footprintsStream = builder.stream(FOOTPRINTS_TOPIC, Consumed.with(Serdes.String(), footprintsDataSerde));
         System.out.println("start streaming processing on topic " + FOOTPRINTS_TOPIC);
 
-        final KStream<String, SubmitEventsRequest> testEventsStream = builder.stream(TEST_EVENTS_TOPIC, Consumed.with(Serdes.String(), testEventsDataSerde));
-        System.out.println("start streaming processing on topic " + TEST_EVENTS_TOPIC);
+//        final KStream<String, SubmitEventsRequest> testEventsStream = builder.stream(TEST_EVENTS_TOPIC, Consumed.with(Serdes.String(), testEventsDataSerde));
+//        System.out.println("start streaming processing on topic " + TEST_EVENTS_TOPIC);
 
-        final KStream<String, String> buildMappingTable = builder.stream(BUILD_TOPIC, Consumed.with(Serdes.String(), buildMappingDataSerde));
+        final KTable<String, BuildMappingRequest> buildMappingTable = builder.table(BUILD_TOPIC, Consumed.with(Serdes.String(), buildMappingDataSerde));
+        KTable<String, Set<String>> flatBuildMappingTable = buildMappingTable.mapValues(buildMapping -> {
+            Set<String> methods = new HashSet<>();
+            for (com.kafka_stream_skeleton.model.proxy.build.FileData fileData: buildMapping.getFiles()) {
+                for (MethodData methodData: fileData.getMethods()) {
+                    methods.add(methodData.getUniqueId());
+                }
+            }
+            return methods;
+        });
 
-        KTable<String, Long> methodHits = footprintsStream
-                .leftJoin(buildMappingTable, (hits, methodKey) -> new MethodKeyHits(methodKey, hits))
-                .map((uniqueId, methodKeyHits) -> new KeyValue<String, Long>(methodKeyHits.getMethodKey(), methodKeyHits.getHits()))
-                .reduceByKey((firstHits, secondHits) -> firstHits + secondHits);
+        KStream<String, KeyValue<String, Integer>> flatFootprintsStream = footprintsStream.flatMapValues(footprintsRequest -> {
+            Map<String, Integer> methodHits = new HashMap<>();
+            for (AppData appData: footprintsRequest.getApps()) {
+                for (FileData fileData: appData.getFiles()) {
+                    for (FootprintsData footprintsData: fileData.getMethods()) {
+                        for (Integer[] hits: footprintsData.getHits()) {
+                            Integer count = methodHits.get(footprintsData.getUniqueId());
+                            if (count == null) {
+                                count = 0;
+                            }
+                            count += hits[1];
+                            methodHits.put(footprintsData.getUniqueId(), count);
+                        }
+                    }
+                }
+            }
+            ArrayList<KeyValue<String, Integer>> m = new ArrayList<>();
+            for (Map.Entry<String, Integer> e: methodHits.entrySet()) {
+                m.add(new KeyValue<>(e.getKey(), e.getValue()));
+            }
+            return m;
+        });
+
+        KStream<String, Integer> coveredUniqueIdHits = flatFootprintsStream.leftJoin(flatBuildMappingTable, (methodHit, uniqueIds) -> {
+            if (uniqueIds.contains(methodHit.key)) {
+                return methodHit.value;
+            }
+            return 0;
+        });
+
     }
-
 
     private static KafkaStreams buildStream() {
         final Properties streamsConfiguration = new Properties();
